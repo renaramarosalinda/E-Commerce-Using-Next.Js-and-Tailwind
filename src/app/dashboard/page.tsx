9b00c6791199541573341be9b57e7d296fc008ac
naaa <89791645+renaramarosalinda@users.noreply.github.com>
2024-02-21 20:01:30 +0700

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,28 +2,102 @@ import React from "react";
 
 export default function Dashboard() {
   return (
-    <div className="carousel rounded-box overflow-hidden">
-      <div className="carousel-inner flex">
-        <div className="carousel-item">
-          <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Burger" className="w-64 h-auto" />
+    <div>
+      <div className="carousel rounded-box overflow-hidden pt-6">
+        <div className="carousel-inner flex">
+          <div className="carousel-item">
+            <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Burger" className="w-64 h-auto" />
+          </div>
+          <div className="carousel-item">
+            <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Burger" className="w-64 h-auto" />
+          </div>
+          <div className="carousel-item">
+            <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Burger" className="w-64 h-auto" />
+          </div>
+          <div className="carousel-item">
+            <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Burger" className="w-64 h-auto" />
+          </div>
+          <div className="carousel-item">
+            <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Burger" className="w-64 h-auto" />
+          </div>
+          <div className="carousel-item">
+            <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Burger" className="w-64 h-auto" />
+          </div>
+          <div className="carousel-item">
+            <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" className="w-64 h-auto" />
+          </div>
         </div>
-        <div className="carousel-item">
-          <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Burger" className="w-64 h-auto" />
+      </div>
+      <div className="grid grid-cols-2 pt-6">
+        <div>
+          <h2 className="text-2xl text-black font-bold text-left pt-6 pb-3">Flash Sales</h2>
         </div>
-        <div className="carousel-item">
-          <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Burger" className="w-64 h-auto" />
+        <div className="flex justify-end">
+          <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+              <span className="countdown font-mono text-5xl">
+                <span></span>
+              </span>
+              days
+            </div>
+            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+              <span className="countdown font-mono text-5xl">
+                <span></span>
+              </span>
+              hours
+            </div>
+            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+              <span className="countdown font-mono text-5xl">
+                <span></span>
+              </span>
+              min
+            </div>
+            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+              <span className="countdown font-mono text-5xl">
+                <span></span>
+              </span>
+              sec
+            </div>
+          </div>
         </div>
-        <div className="carousel-item">
-          <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Burger" className="w-64 h-auto" />
+      </div>
+
+      <div className="py-6 flex flex-wrap justify-between">
+        <div className="card w-96 bg-base-100 shadow-xl mb-4">
+          <figure>
+            <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
         </div>
-        <div className="carousel-item">
-          <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Burger" className="w-64 h-auto" />
+        <div className="card w-96 bg-base-100 shadow-xl mb-4">
+          <figure>
+            <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
         </div>
-        <div className="carousel-item">
-          <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Burger" className="w-64 h-auto" />
-        </div>
-        <div className="carousel-item">
-          <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" className="w-64 h-auto" />
+        <div className="card w-96 bg-base-100 shadow-xl mb-4">
+          <figure>
+            <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
