c47b99b6720fbe4519f4676f61335036c1f996ad
naaa <89791645+renaramarosalinda@users.noreply.github.com>
2024-02-22 21:25:32 +0700

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,25 +6,9 @@ export default function Dashboard() {
       <div className="carousel rounded-box overflow-hidden pt-6">
         <div className="carousel-inner flex">
           <div className="carousel-item">
-            <img src="/img/Frame 560.png" alt="Burger" className="w-64 h-auto" />
-          </div>
-          <div className="carousel-item">
-            <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Burger" className="w-64 h-auto" />
-          </div>
-          <div className="carousel-item">
-            <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Burger" className="w-64 h-auto" />
-          </div>
-          <div className="carousel-item">
-            <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Burger" className="w-64 h-auto" />
-          </div>
-          <div className="carousel-item">
-            <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Burger" className="w-64 h-auto" />
-          </div>
-          <div className="carousel-item">
-            <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Burger" className="w-64 h-auto" />
-          </div>
-          <div className="carousel-item">
-            <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" className="w-64 h-auto" />
+            <div className="item-center">
+              <img src="/img/Frame 560.png" alt="Burger" className="carousel-image" />
+            </div>
           </div>
         </div>
       </div>
@@ -35,25 +19,25 @@ export default function Dashboard() {
         <div className="flex justify-end">
           <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
             <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-              <span className="countdown font-mono text-5xl">
+              <span className="countdown font-mono text-2xl">
                 <span></span>
               </span>
               days
             </div>
             <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-              <span className="countdown font-mono text-5xl">
+              <span className="countdown font-mono text-2xl">
                 <span></span>
               </span>
               hours
             </div>
             <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-              <span className="countdown font-mono text-5xl">
+              <span className="countdown font-mono text-2xl">
                 <span></span>
               </span>
               min
             </div>
             <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-              <span className="countdown font-mono text-5xl">
+              <span className="countdown font-mono text-2xl">
                 <span></span>
               </span>
               sec
@@ -68,10 +52,10 @@ export default function Dashboard() {
             <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className="card-title">$400</h2>
+            <p></p>
             <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+              <button className="btn btn-primary btn-outline">Buy Now</button>
             </div>
           </div>
         </div>
@@ -80,10 +64,10 @@ export default function Dashboard() {
             <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className="card-title">$400</h2>
+            <p></p>
             <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+              <button className="btn btn-primary btn-outline">Buy Now</button>
             </div>
           </div>
         </div>
@@ -92,10 +76,10 @@ export default function Dashboard() {
             <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className="card-title">$400</h2>
+            <p></p>
             <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+              <button className="btn btn-primary btn-outline">Buy Now</button>
             </div>
           </div>
         </div>
